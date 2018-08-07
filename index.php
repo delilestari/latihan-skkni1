05306d48a2a5725b39aa4b4a92c12c0bce743089
adeavenged <dheendra7393@gmail.com>
2018-08-07 20:22:11 +0700

    version 2.0

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Selamat Datang</title>
-</head>
-<body>
-	<a href="form-aritmatika.php">Soal Aritmatika (Soal 1)</a>
-</body>
-</html>
+<?php 
+	header("location:form-aritmatika.php");
+?>
